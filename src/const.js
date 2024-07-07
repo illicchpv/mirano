ea7734d933e6de9721c71e41b6e36108847bce3f
illicchpv <illich58@mail.ru>
2024-07-07 20:08:44 +0300

    опубликовал сервер;

--- src/const.js
+++ src/const.js
@@ -1,1 +1,6 @@
-export const API_URL = 'https://mirano-api.onrender.com';
+// сервер Максима
+// export const API_URL = 'https://mirano-api.onrender.com';
+
+// мой сервер
+export const API_URL = 'https://mirano-api-4txw.onrender.com';
+// swagger https://mirano-api-4txw.onrender.com/api-docs
