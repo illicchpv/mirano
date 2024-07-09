aca6a0789ad6e112c50d836a5a43764453dc184f
illicchpv <illich58@mail.ru>
2024-07-09 06:52:54 +0300

    скролл до карзины при открытии+

--- src/modules/Cart/Cart.jsx
+++ src/modules/Cart/Cart.jsx
@@ -5,64 +5,72 @@ import {CartItem} from '../CartItem/CartItem';
 import {useDispatch, useSelector} from 'react-redux';
 import {toggleCart} from '../../redux/cartSlice';
 import {openOrder} from '../../redux/orderSlice';
+import {useEffect, useRef} from 'react';
 
 export function Cart() {
   const dispatch = useDispatch();
   const isOpen = useSelector((state) => state.cart.isOpen);
   const items = useSelector((state) => state.cart.items);
-    
-    if(!isOpen) return null;
-    
-    const handlerCartToggle = () => {
-        dispatch(toggleCart());
-    };
-    const handlerOrderOpen = () => {
-        dispatch(openOrder());
-    };
+  const cartRef = useRef(null);
 
-    return (<>
+  const handlerCartToggle = () => {
+    dispatch(toggleCart());
+  };
+  const handlerOrderOpen = () => {
+    dispatch(openOrder());
+  };
 
-        <section className="cart cart_open">
-            <div className="cart__container">
-                <div className="cart__header">
-                    <h3 className="cart__title">Ваш заказ</h3>
+  useEffect(() => {
+    if (isOpen) {
+      cartRef.current.scrollIntoView({block: 'start', behavior: 'smooth'});
+    }
+  }, [isOpen]);
 
-                    <button className="cart__close"
-                        onClick={handlerCartToggle}
-                    >
-                        <svg width="28" height="28" viewBox="0 0 28 28" fill="none"
-                            xmlns="http://www.w3.org/2000/svg">
-                            <rect x="5" y="5.70715" width="1" height="25"
-                                transform="rotate(-45 5 5.70715)" fill="#D17D2F" />
-                            <rect x="22.6777" y="5" width="1" height="25"
-                                transform="rotate(45 22.6777 5)" fill="#D17D2F" />
-                        </svg>
-                    </button>
-                </div>
+  if (!isOpen) return null;
 
-                <p className="cart__date-delivery">сегодня в 14:00</p>
+  return (<>
 
-                <ul className="cart__list">
+    <section className="cart cart_open" ref={cartRef}>
+      <div className="cart__container">
+        <div className="cart__header">
+          <h3 className="cart__title">Ваш заказ</h3>
 
-                    {items.map((el) => {
+          <button className="cart__close"
+            onClick={handlerCartToggle}
+          >
+            <svg width="28" height="28" viewBox="0 0 28 28" fill="none"
+              xmlns="http://www.w3.org/2000/svg">
+              <rect x="5" y="5.70715" width="1" height="25"
+                transform="rotate(-45 5 5.70715)" fill="#D17D2F" />
+              <rect x="22.6777" y="5" width="1" height="25"
+                transform="rotate(45 22.6777 5)" fill="#D17D2F" />
+            </svg>
+          </button>
+        </div>
 
-                        return (
+        <p className="cart__date-delivery">сегодня в 14:00</p>
 
-                            <CartItem key={el.id} {...el} />
+        <ul className="cart__list">
 
-                        );
-                    })}
+          {items.map((el) => {
 
-                </ul>
+            return (
 
-                <div className="cart__footer">
-                    <button className="cart__order-btn"
-                        onClick={handlerOrderOpen}
-                    >Оформить</button>
-                    <p className="cart__price cart__price_total">0&nbsp;₽</p>
-                </div>
-            </div>
-        </section>
+              <CartItem key={el.id} {...el} />
 
-    </>);
+            );
+          })}
+
+        </ul>
+
+        <div className="cart__footer">
+          <button className="cart__order-btn"
+            onClick={handlerOrderOpen}
+          >Оформить</button>
+          <p className="cart__price cart__price_total">0&nbsp;₽</p>
+        </div>
+      </div>
+    </section>
+
+  </>);
 }
