8bf2c0da2d82ef069b964d251a0d904ffb8330d6
illicchpv <illich58@mail.ru>
2024-07-11 10:12:05 +0300

    +

--- src/modules/Filter/Filter.jsx
+++ src/modules/Filter/Filter.jsx
@@ -43,7 +43,7 @@ export function Filter({setTitleGods}) {
       debFetchGoods(validFilters);
     }
     prevFiltersRef.current = validFilters;
-  }, [filters, dispatch, debFetchGoods]);
+  }, [setTitleGods, filters, dispatch, debFetchGoods]);
 
   const handleChoicesToggle = (v) => {
     setOpenChoice(p => {
