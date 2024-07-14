e42d864348459bab237707fa7438b4a8ae577890
illicchpv <illich58@mail.ru>
2024-07-14 12:57:49 +0300

    правки в корзине.

--- src/modules/CartItem/CartItem.jsx
+++ src/modules/CartItem/CartItem.jsx
@@ -1,18 +1,20 @@
+import {API_URL} from '../../const';
 import style from './CartItem.module.scss';
 
-export function CartItem({img, title, price}) {
+export function CartItem({id, photoUrl, name, price, quantity}) {
+  console.log('id: ', id);
 
     return (<>
         <li className={style.item}>
             <img className={style.img}
-                src={img}
-                alt="Букет из роз Grand Avalanche (101 шт)" />
-            <h4 className={style.title}>{title}</h4>
+                src={`${API_URL}${photoUrl}`}
+                alt={name} />
+            <h4 className={style.title}>{name}</h4>
             <div className={style.counter}><button
                 className={style.btn}>-</button>
                 <input
                     className={style.input} type="number" max="99" min="0"
-                    value="1" />
+                    value={quantity} />
                 <button className={style.btn}>+</button></div>
             <p className={style.price}>{price}&nbsp;₽</p>
         </li>
