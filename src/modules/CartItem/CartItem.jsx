4c282c1bb2d01acac0c512167817f123d91ca9c1
illicchpv <illich58@mail.ru>
2024-07-14 13:20:09 +0300

    в CartItem управление числом товаров+

--- src/modules/CartItem/CartItem.jsx
+++ src/modules/CartItem/CartItem.jsx
@@ -1,23 +1,53 @@
-import {API_URL} from '../../const';
+import {useDispatch} from 'react-redux';
+import {API_URL, debounce} from '../../const';
 import style from './CartItem.module.scss';
+import {useState} from 'react';
+import {addItemToCart} from '../../redux/cartSlice';
 
 export function CartItem({id, photoUrl, name, price, quantity}) {
   console.log('id: ', id);
+  const dispatch = useDispatch();
+  const [inputQuantity, setInputQuantity] = useState(quantity);
 
-    return (<>
-        <li className={style.item}>
-            <img className={style.img}
-                src={`${API_URL}${photoUrl}`}
-                alt={name} />
-            <h4 className={style.title}>{name}</h4>
-            <div className={style.counter}><button
-                className={style.btn}>-</button>
-                <input
-                    className={style.input} type="number" max="99" min="0"
-                    value={quantity} />
-                <button className={style.btn}>+</button></div>
-            <p className={style.price}>{price}&nbsp;₽</p>
-        </li>
+  const debouncedInputChange = debounce((v) => dispatch(addItemToCart({productId: id, quantity: v})), 500);
 
-    </>);
+  const handleInputChange = (event) => {
+    const v = +event.target.value;
+    setInputQuantity(v);
+    dispatch(addItemToCart({productId: id, quantity: v}));
+    debouncedInputChange(v)
+  };
+  const handleDec = () => {
+    const v = inputQuantity - 1;
+    setInputQuantity(v);
+    debouncedInputChange(v)
+  };
+  const handleInc = () => {
+    const v = inputQuantity + 1;
+    setInputQuantity(v);
+    debouncedInputChange(v)
+  };
+
+  return (<>
+    <li className={style.item}>
+      <img className={style.img}
+        src={`${API_URL}${photoUrl}`}
+        alt={name} />
+      <h4 className={style.title}>{name}</h4>
+      <div className={style.counter}>
+        <button className={style.btn}
+          onClick={handleDec}
+        >-</button>
+        <input
+          className={style.input} type="number" max="99" min="0"
+          value={inputQuantity}
+          onChange={handleInputChange}
+        />
+        <button className={style.btn}
+          onClick={handleInc}
+        >+</button></div>
+      <p className={style.price}>{price*quantity}&nbsp;₽</p>
+    </li>
+
+  </>);
 }
