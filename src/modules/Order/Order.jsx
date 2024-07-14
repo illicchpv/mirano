e363dd6e03071e49991560bd470e4847cd022068
illicchpv <illich58@mail.ru>
2024-07-14 17:29:53 +0300

    форма и отправка заказа

--- src/modules/Order/Order.jsx
+++ src/modules/Order/Order.jsx
@@ -1,17 +1,29 @@
 import {useDispatch, useSelector} from 'react-redux';
 import style from './Order.module.scss';
-import {closeOrder} from '../../redux/orderSlice';
+import {closeOrder, sentOrder, updateOrderData} from '../../redux/orderSlice';
 import classNames from 'classnames';
 import {useCallback, useEffect} from 'react';
 
 export function Order() {
   const dispatch = useDispatch();
-  const isOrderReady = false;
   const isOpen = useSelector((state) => state.order.isOpen);
+  const orderId = useSelector((state) => state.order.orderId);
+  const orderData = useSelector((state) => state.order.data);
+  const itemsCart = useSelector((state) => state.cart.items);
 
-  const handlerOrderClose = useCallback( () => {
+  const handlerOrderClose = useCallback(() => {
     dispatch(closeOrder());
   }, [dispatch]);
+
+  const handleChange = (e) => {
+    const {name, value} = e.target;
+    dispatch(updateOrderData({[name]: value, }));
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    dispatch(sentOrder());
+  };
 
   useEffect(() => {
     const handleEscape = (e) => {
@@ -36,24 +48,32 @@ export function Order() {
 
     <div className={style.order} onClick={handlerOrderClose}>
       <div className={style.wrapper} onClick={e => e.stopPropagation()}>
-        {isOrderReady ?
+        {orderId ?
           (<>
 
             <h2 className={style.title}>Заказ оформлен!</h2>
-            <p className={style.id}>Ваш номер заказа:
-              971f365a-caa1-4cdb-9446-bad2eff047e1</p>
+            <p className={style.id}>Ваш номер заказа: {orderId}</p>
 
           </>) : (<>
 
             <h2 className={style.title}>Оформить заказ</h2>
-            <form className={style.form} id="order">
+            <form className={style.form} id="order"
+              onSubmit={handleSubmit}
+            >
               <fieldset className={style.fieldset}>
                 <legend className={style.legend}>Данные заказчика</legend>
                 <div className={style["input-group"]}>
                   <input className={style.input}
-                    type="text" name="name-buyer" placeholder="Имя" />
+                    type="text"
+                    name="buyerName"
+                    value={orderData.buyerName}
+                    onChange={handleChange}
+                    placeholder="Имя" />
                   <input
-                    className={style.input} type="text" name="phone-buyer"
+                    className={style.input} type="text"
+                    name="buyerPhone"
+                    value={orderData.buyerPhone}
+                    onChange={handleChange}
                     placeholder="Телефон" />
                 </div>
               </fieldset>
@@ -61,9 +81,16 @@ export function Order() {
                 <legend className={style.legend}>Данные получателя</legend>
                 <div className={style["input-group"]}>
                   <input className={style.input}
-                    type="text" name="name-recipient" placeholder="Имя" />
+                    type="text"
+                    name="recipientName"
+                    value={orderData.recipientName}
+                    onChange={handleChange}
+                    placeholder="Имя" />
                   <input
-                    className={style.input} type="text" name="phone-recipient"
+                    className={style.input} type="text"
+                    name="recipientPhone"
+                    value={orderData.recipientPhone}
+                    onChange={handleChange}
                     placeholder="Телефон" />
                 </div>
               </fieldset>
@@ -71,37 +98,64 @@ export function Order() {
                 <legend className={style.legend}>Адрес</legend>
                 <div className={style["input-group"]}>
                   <input className={style.input}
-                    type="text" name="street" placeholder="Улица" />
+                    type="text"
+                    name="street"
+                    value={orderData.street}
+                    onChange={handleChange}
+                    placeholder="Улица" />
 
                   <input className={classNames(style.input, style.input_min)}
-                    type="text" name="house" placeholder="Дом" />
+                    type="text"
+                    name="house"
+                    value={orderData.house}
+                    onChange={handleChange}
+                    placeholder="Дом" />
 
                   <input className={classNames(style.input, style.input_min)}
-                    type="text" name="apartment" placeholder="Квартира" />
+                    type="text"
+                    name="apartment"
+                    value={orderData.apartment}
+                    onChange={handleChange}
+                    placeholder="Квартира" />
                 </div>
               </fieldset>
+
               <fieldset className={style.fieldset}>
                 <div className={style.payment}><label className={style["label-radio"]}>
                   <input
-                    className={style.radio} type="radio" name="payment-online"
-                    value="true" defaultChecked />Оплата онлайн</label></div>
-                <div className={style.delivery}><label htmlFor="delivery">Доставка
-                  01.07</label>
-                  <input type="hidden" name="delivery-date"
-                    value="01.07" />
-                  <div className={style["select-wrapper"]}><select className={style.select}
-                    name="delivery-time" id="delivery">
-                    <option value="9-12">с 9:00 до 12:00</option>
-                    <option value="12-15">с 12:00 до 15:00</option>
-                    <option value="15-18">с 15:00 до 18:00</option>
-                    <option value="18-21">с 18:00 до 21:00</option>
-                  </select></div>
+                    className={style.radio} type="radio"
+                    name="paymentOnline"
+                    value={orderData.paymentOnline === 'true'}
+                    onChange={handleChange}
+                    defaultChecked />Оплата онлайн</label>
+                </div>
+                <div className={style.delivery}>
+                  <label htmlFor="delivery">Доставка 01.07</label>
+                  <input type="hidden"
+                    name="deliveryDate"
+                    value={orderData.deliveryDate}
+                    onChange={handleChange}
+                  />
+                  <div className={style["select-wrapper"]}>
+                    <select className={style.select}
+                      name="deliveryTime"
+                      value={orderData.deliveryTime}
+                      onChange={handleChange}
+                      id="delivery">
+                      <option value="9-12">с 9:00 до 12:00</option>
+                      <option value="12-15">с 12:00 до 15:00</option>
+                      <option value="15-18">с 15:00 до 18:00</option>
+                      <option value="18-21">с 18:00 до 21:00</option>
+                    </select></div>
                 </div>
               </fieldset>
             </form>
             <div className={style.footer}>
-              <p className={style.total}>92100&nbsp;₽</p><button className={style.button}
-                type="submit" form="order">Заказать</button>
+              <p className={style.total}>
+                {itemsCart.reduce((acc, el) => acc + el.price * el.quantity, 0)}
+                &nbsp;₽
+              </p>
+              <button className={style.button} type="submit" form="order">Заказать</button>
             </div>
 
           </>)}
--- src/redux/orderSlice.js
+++ src/redux/orderSlice.js
@@ -1,22 +1,113 @@
-import {createSlice} from '@reduxjs/toolkit';
+import {createAsyncThunk, createSlice} from '@reduxjs/toolkit';
+import {API_URL} from '../const';
+import {fetchCart, toggleCart} from './cartSlice';
 
 const initialState = {
-    isOpen: false,
+  isOpen: false,
+  orderId: '',
+  data: {
+    buyerName: '',
+    buyerPhone: '',
+    recipientName: '',
+    recipientPhone: '',
+    street: '',
+    house: '',
+    apartment: '',
+
+    paymentOnline: 'true',
+    deliveryDate: '',
+    deliveryTime: '',
+  },
 };
 
+export const sendOrder = createAsyncThunk("order/sendOrder", async (_, {getState, dispatch}) => {
+  // const state = getState();
+  const {order: {data: {
+    buyerName,
+    buyerPhone,
+    recipientName,
+    recipientPhone,
+    street,
+    house,
+    apartment,
+
+    paymentOnline,
+    deliveryDate,
+    deliveryTime,    
+  }}} = getState();
+  const orderData = {
+    "buyer": {
+      "name": buyerName,
+      "phone": buyerPhone,
+    },
+    "recipient": {
+      "name": recipientName,
+      "phone": recipientPhone
+    },
+    "address": `${street}, ${house}, ${apartment}`,
+    paymentOnline,
+    deliveryDate,
+    deliveryTime,
+  };
+
+  const resp = await fetch(`${API_URL}/api/orders`, {
+    method: 'POST',
+    credentials: 'include',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(orderData),
+  });
+  if (!resp.ok) {
+    throw new Error('sendOrder response error');
+  }
+
+  dispatch(clearOrder());
+  dispatch(toggleCart());
+  dispatch(fetchCart());
+
+  return await resp.json();
+
+});
+
+
+
 const orderSlice = createSlice({
-    name: 'order',
-    initialState,
-    reducers: {
-        openOrder(state, action) {
-            console.log('openOrder action: ', action);
-            state.isOpen = true;
-        },
-        closeOrder(state, action) {
-            console.log('closeOrder action: ', action);
-            state.isOpen = false;
-        }
-    }
+  name: 'order',
+  initialState,
+  reducers: {
+    openOrder(state, action) {
+      console.log('openOrder action: ', action);
+      state.isOpen = true;
+    },
+    closeOrder(state, action) {
+      console.log('closeOrder action: ', action);
+      state.isOpen = false;
+    },
+    clearOrder(state, action) {
+      console.log('clearOrder action: ', action);
+      state.data = initialState.data;
+    },
+    updateOrderData(state, action) {
+      console.log('updateOrderData action: ', action);
+      //??? state.data[action.payload.name] = action.payload.value;
+      state.data = {...state.data, ...action.payload};
+    },
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(sendOrder.pending, (state) => {
+        state.status = 'loading';
+      })
+      .addCase(sendOrder.fulfilled, (state, action) => {
+        state.status = 'succeeded';
+        state.orderId = action.payload.id;
+      })
+      .addCase(sendOrder.rejected, (state, action) => {
+        state.status = 'failed';
+        state.error = action.error.message;
+      });
+  },
 });
 export default orderSlice.reducer;
-export const {openOrder, closeOrder} = orderSlice.actions;
+export const {openOrder, clearOrder, closeOrder, updateOrderData} = orderSlice.actions;
