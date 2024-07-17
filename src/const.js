c99a0c5a67f6d4e4901ede844425f038f4a65e09
illicchpv <illich58@mail.ru>
2024-07-17 18:08:47 +0300

    используем adjustElementPosition чтоб не вылезали choices.

--- src/const.js
+++ src/const.js
@@ -36,4 +36,35 @@ export const debounce = (fn, ms) => {
 
 export const isNumber = (n) => {
   return !isNaN(parseInt(n)) && isFinite(n);
-}
+};
+
+export const adjustElementPosition = (element, count = 0) => {
+  const rect = element.getBoundingClientRect();
+  const viewportWidth = window.innerWidth;
+
+  if (rect.left < 0) {
+    element.style.cssText = `
+      left: 0;
+      right: auto;
+      transform: translateX(0)
+    `;
+  } else if (rect.right > viewportWidth) {
+    element.style.cssText = `
+      left: auto;
+      right: 0;
+      transform: translateX(0)
+    `;
+  } else {
+    element.style.cssText = `
+      left: 50%;
+      right: auto;
+      transform: translateX(-50%)
+    `;
+  }
+
+  const postRect = element.getBoundingClientRect();
+
+  if ((postRect.left < 0 || postRect.right > viewportWidth) && count > 3) {
+    adjustElementPosition(element, ++count);
+  }
+};
--- src/modules/Choices/Choices.jsx
+++ src/modules/Choices/Choices.jsx
@@ -1,13 +1,31 @@
+import {useRef} from 'react';
 import './choices.scss';
+import {useEffect} from 'react';
+import {adjustElementPosition, debounce} from '../../const';
 
 export function Choices({children, buttonLabel, className, isOpen, handleChoicesToggle}) {
+  const choiceRef = useRef(null);
 
-    return (<>
-        <div className={`${['choices', className].filter(el => el).join(' ')}`}>
-            <button className="choices__btn" onClick={handleChoicesToggle}
-                type="button">{buttonLabel}</button>
+  useEffect(() => {
+    if (isOpen) {
+      adjustElementPosition(choiceRef.current);
+    }
+    const debAdjustElementPosition = debounce(() => {
+      if (isOpen) {
+        adjustElementPosition(choiceRef.current);
+      }
+    }, 100);
 
-            {isOpen && <div className="choices__box">{children}</div>}
-        </div>
-    </>);
+    window.addEventListener('resize', debAdjustElementPosition);
+    return () => window.removeEventListener('resize', debAdjustElementPosition);
+  }, [isOpen]);
+
+  return (<>
+    <div className={`${['choices', className].filter(el => el).join(' ')}`}>
+      <button className="choices__btn" onClick={handleChoicesToggle}
+        type="button">{buttonLabel}</button>
+
+      {isOpen && <div className="choices__box" ref={choiceRef}>{children}</div>}
+    </div>
+  </>);
 }
